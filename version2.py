31851f2289c20718659b11c849962e35b7c40080
robertcaroli <robertcaroli58@gmail.com>
2016-10-21 19:15:25 +0100

    Add files via upload

---
+++ version2.py
@@ -0,0 +1,8 @@
+name= print("Hi there")
+input("What is your name?")
+input("Nice to meet you!")
+emotion = input("How are you?")
+print("I am glad that you are",emotion)
+distance = input ("Where do you live?")
+distance = input ("How far is Croydon from your school?")
+input ("That is a big distance that you have to travel")
